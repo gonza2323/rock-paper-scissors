a8dd49c721b978d80c36c859e390f9aba5b81216
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-02 02:50:36 -0300

    Implement game() function

--- javascript.js
+++ javascript.js
@@ -16,24 +16,41 @@ function playRound(playerSelection, computerSelection) {
     let result = (playerSelection - computerSelection + 3) % 3;
 
     if (result == 0) {
-        return "It's a tie!";
+        console.log("It's a tie!");
+        return 0;
     }
 
     if (result == 1) {
-        return `You win! ${OPTIONS[playerSelection]} beats ${OPTIONS[computerSelection]}`;
+        console.log(`You win! ${OPTIONS[playerSelection]} beats ${OPTIONS[computerSelection]}`);
+        return 1;
     }
     
     if (result == 2) {
-        return `You lose! ${OPTIONS[computerSelection]} beats ${OPTIONS[playerSelection]}`;
+        console.log(`You lose! ${OPTIONS[computerSelection]} beats ${OPTIONS[playerSelection]}`);
+        return -1;
     }
 }
 
-console.log(playRound(0,0));
-console.log(playRound(0,1));
-console.log(playRound(0,2));
-console.log(playRound(1,0));
-console.log(playRound(1,1));
-console.log(playRound(1,2));
-console.log(playRound(2,0));
-console.log(playRound(2,1));
-console.log(playRound(2,2));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult;
+    let gameResult;
+
+    for (let i = 0; i < 5; i++) {
+        roundResult = playRound(getPlayerChoice(), getComputerChoice());
+        playerScore += Math.max(roundResult, 0);
+        computerScore -= Math.min(roundResult, 0);
+        console.log(`Player Score: ${playerScore}\tComputer Score ${computerScore}`);
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Player wins the game!");
+    }
+    else if (playerScore < computerScore) {
+        console.log("Computer wins the game!");
+    }
+    else {
+        console.log("It's a tie! No one won!");
+    }
+}
