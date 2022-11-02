48b60fabfb9b935de5d5aa44c3579f07c21dba3a
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-02 01:49:43 -0300

    Change getComputerChoice() return type

--- javascript.js
+++ javascript.js
@@ -1,7 +1,36 @@
 "use strict";
 
-function getComputerChoice () {
-    const options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random()*3)];
+const options = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return "It's a tie!";
+        } else if (computerSelection == "paper") {
+            return "You lose! Paper beats rock";
+        } else if (computerSelection == "scissors") {
+            return "You win! Rock beats scissors";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You win! Paper beats rock";
+        } else if (computerSelection == "paper") {
+            return "It's a tie!";
+        } else if (computerSelection == "scissors") {
+            return "You lose! Scissors beats paper";
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "You lose! Rock beats scissors";
+        } else if (computerSelection == "paper") {
+            return "You win! Scissors beats paper";
+        } else if (computerSelection == "scissors") {
+            return "It's a tie!";
+        }
+    }
 }
 
