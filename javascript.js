fc475522ae699f7f9b4412111d58e08ed9a03488
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-02 02:29:17 -0300

    Implement playRound() function

--- javascript.js
+++ javascript.js
@@ -13,8 +13,27 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let result = (playerSelection - computerSelection + 3) % 3;
+
+    if (result == 0) {
+        return "It's a tie!";
+    }
+
+    if (result == 1) {
+        return `You win! ${OPTIONS[playerSelection]} beats ${OPTIONS[computerSelection]}`;
+    }
+    
+    if (result == 2) {
+        return `You lose! ${OPTIONS[computerSelection]} beats ${OPTIONS[playerSelection]}`;
+    }
 }
 
-while (true){
-    console.log(getPlayerChoice());
-}
+console.log(playRound(0,0));
+console.log(playRound(0,1));
+console.log(playRound(0,2));
+console.log(playRound(1,0));
+console.log(playRound(1,1));
+console.log(playRound(1,2));
+console.log(playRound(2,0));
+console.log(playRound(2,1));
+console.log(playRound(2,2));
